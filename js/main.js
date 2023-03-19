45aa53cb3f241507b5fa230eabc997e7cf8501d8
Marbax <macintosh@MacBook-Air.local>
2023-03-19 16:06:16 +0100

    update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,8 @@ const emptyList = document.querySelector('#emptyList')
 
 let tasks = []
 
-// проверка localStorage при загрузке приложения, если в нем есть данные тогда поместить их в список дел
 if (localStorage.getItem('tasks')) {
 	tasks = JSON.parse(localStorage.getItem('tasks'))
-	// рендер задач
 	tasks.forEach((task) => renderTask(task))
 }
 
@@ -19,60 +17,35 @@ tasksList.addEventListener('click', deleateTask)
 tasksList.addEventListener('click', doneTask)
 
 function addTask(event) {
-	// отмена отправки формы
 	event.preventDefault()
 
-
-	// достаем текст задачи из поля ввода
 	const taskText = taskInput.value
 
-
-	// описываем задачу в виде объекта
 	const newTask = {
 		id: Date.now(),
 		text: taskText,
 		done: false
 	}
 
-	// добавляем задачу в массив с задачами
 	tasks.push(newTask)
 
-	// сохраняем списки задач в хранилище браузера LocalStorage
 	saveToLocalStorage()
 
 	renderTask(newTask)
 
-	// очищаем поле ввода и возвращаем на него фокус
 	taskInput.value = ''
 	taskInput.focus()
 
 	checkEmptyList()
-
-	//saveHTMLtoLS()
 }
 
 function deleateTask() {
-	// проверяем что клик был НЕ по кнопке "удалить задачу"
 	if (event.target.dataset.action !== 'delete') return
 
 	const parentNode = event.target.closest('.list-group-item')
 
-	//  определяем ID задачи(и присвоить его к числу)
 	const id = Number(parentNode.id)
 
-	/*
-	// * (1) находим индекс задачи в массиве
-	const index = tasks.findIndex((task) => {
-		if (task.id === id) {
-			return true
-		}
-	})
-
-	// удаляем задачу из массива
-	tasks.splice(index, 1)
-	*/
-
-	// ! (1) либо с помощью метода filter / Удаляем задачу через фильтрацию массива
 	tasks = tasks.filter((task) => {
 		if (task.id === id) {
 			return false
@@ -81,15 +54,11 @@ function deleateTask() {
 		}
 	})
 
-	// сохраняем списки задач в хранилище браузера LocalStorage
 	saveToLocalStorage()
 
-	// удаляем задачу из разметки
 	parentNode.remove()
 
 	checkEmptyList()
-
-	//saveHTMLtoLS()
 }
 
 
@@ -98,29 +67,22 @@ function doneTask(event) {
 
 	const parentNode = event.target.closest('.list-group-item')
 
-	// определяем ID задачи
 	const id = Number(parentNode.id)
 
-	// если функция при иттерации цикла на каком то моменте возвращает true, считается что этот елемент найден и он будет возвращен
 	const task = tasks.find((task) => {
 		if (task.id === id) {
 			return true
 		}
 	})
 
-	// task.done равняется обратному значению task.done
 	task.done = !task.done
 
-	// сохраняем списки задач в хранилище браузера LocalStorage
 	saveToLocalStorage()
 
 	const taskTitle = parentNode.querySelector('.task-title')
 	taskTitle.classList.toggle('task-title--done')
 
 	checkEmptyList()
-
-
-	//saveHTMLtoLS()
 }
 
 function checkEmptyList() {
@@ -140,18 +102,13 @@ function checkEmptyList() {
 	}
 }
 
-// сохраняем списки задач в хранилище браузера LocalStorage
 function saveToLocalStorage() {
 	localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-// Рендерим задачу на странице
 function renderTask(task) {
-	// формируем css класс
 	const cssClass = task.done ? 'task-title task-title--done' : 'task-title'
 
-
-	// формируемразметку для новой задачи
 	const taskHTML = `
 	<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
 		<span class="${cssClass}">${task.text}</span>
@@ -166,16 +123,5 @@ function renderTask(task) {
 	</li>
 	`
 
-	// добавляем задачу на страницу
 	tasksList.insertAdjacentHTML('beforeend', taskHTML)
 }
-
-
-// ! АНТИ паттерн с записью в локал сторейдж всей разметки списка
-// if (localStorage.getItem('tasksHTML')) {
-// 	tasksList.innerHTML = localStorage.getItem('tasksHTML')
-// }
-
-// function saveHTMLtoLS() {
-// 	localStorage.setItem('tasksHTML', tasksList.innerHTML)
-// }
